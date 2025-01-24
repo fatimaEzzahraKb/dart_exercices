b76067f14a58d4f45f360e5ccb1f4d61aade094d
Fatima-ezzahra ELKAWKABI <fatizahra.elkawkabi@gmail.com>
2025-01-24 18:01:14 +0100

    ex9_suite

--- EX9.dart
+++ EX9.dart
@@ -1,1 +1,14 @@
-int findMaximum(List<int> numbers) {}
+int findMaximum(List<int> numbers) {
+  int max = numbers[0];
+  for (int i = 1; i < numbers.length; i++) {
+    if (numbers[i] > max) {
+      max = numbers[i];
+    }
+  }
+  return max;
+}
+
+void main() {
+  List<int> mynums = [20, 1, 2, 3, 18];
+  print(findMaximum(mynums));
+}
